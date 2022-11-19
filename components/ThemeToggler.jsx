4a48748729938784ac5e4272a45be04967b5bd35
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-19 11:57:05 +0530

    theme toggler added

---
+++ components/ThemeToggler.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const ThemeToggler = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <button onClick={handleClick} className="h-10 w-10 dark:bg-zinc-300 rounded-full bg-rose-500">
+      {theme === "light" ? "🌙" : "🌞"}
+    </button>
+  );
+};
+
+export default ThemeToggler;
