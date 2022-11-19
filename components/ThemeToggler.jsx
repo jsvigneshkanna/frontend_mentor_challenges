249f3443bd13b8efbeeb2d7e020a86d52d57cb27
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-19 12:38:37 +0530

    nextjs themetoggler added

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,25 +1,44 @@
-import { useState, useEffect } from "react";
+import useDarkMode from "../hook/useDarkMode";
 
 const ThemeToggler = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
-
-  const handleClick = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("theme", theme);
-  }, [theme]);
+  const [colorTheme, setTheme] = useDarkMode();
 
   return (
-    <button onClick={handleClick} className="h-10 w-10 dark:bg-zinc-300 rounded-full bg-rose-500">
-      {theme === "light" ? "🌙" : "🌞"}
-    </button>
+    <div>
+      {colorTheme === "light" ? (
+        <svg
+          onClick={() => setTheme("light")}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-indigo-200"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
+      ) : (
+        <svg
+          onClick={() => setTheme("dark")}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-gray-900"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      )}
+    </div>
   );
 };
 
