72f7e4816ef879694281604ea71ffa597964c817
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-27 19:08:27 +0530

    footer added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-slate-300 min-h-screen dark:bg-slate-800 text-zinc-800 dark:text-white scroll-smooth">
       <Navbar />
-      <Component {...pageProps} />
+      <div className="pb-2 md:pb-20">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ pages/solutions/interactive_rating_component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const interactive_rating_component = () => {
+  return <div>interactive_rating_component</div>;
+};
+
+export default interactive_rating_component;
