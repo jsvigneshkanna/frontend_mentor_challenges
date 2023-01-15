ecf55b1d91ae1acdd92792b3cc4546bd4cfa8df9
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-15 14:59:53 +0530

    nft preview card images stored

--- challenge_solutions/nft_preview_card_component.jsx
+++ challenge_solutions/nft_preview_card_component.jsx
@@ -1,5 +1,9 @@
 const NftPreviewCard = () => {
-  return <div>nft_preview_card_component</div>;
+  return (
+    <div className="bg-white dark:bg-slate-900 rounded-xl text-black shadow-sm shadow-gray-400 dark:shadow-slate-600 p-4 w-[270px] md:w-[320px]">
+      faf
+    </div>
+  );
 };
 
 export default NftPreviewCard;
