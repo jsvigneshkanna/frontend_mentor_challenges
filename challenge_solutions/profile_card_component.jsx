2d148c29bbbabcc449bff644f63fa619f55343ba
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-04 21:51:29 +0530

    profile card final commit

--- challenge_solutions/profile_card_component.jsx
+++ challenge_solutions/profile_card_component.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ProfileCardComponent = () => {
-  return <div>profile_card_component</div>;
+  return (
+    <div className="w-[270px] md:w-[320px]  p-4 rounded-xl overflow-hidden">
+      {/* Top SVG which has bubble images in greeen */}
+      <div className="h-24 bg-[#23a3af]"></div>
+    </div>
+  );
 };
 
 export default ProfileCardComponent;
