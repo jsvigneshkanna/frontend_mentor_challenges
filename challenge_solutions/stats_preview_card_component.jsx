205933d659d22d51d7b99cdf2cfbf423b37c88fc
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-06 00:39:15 +0530

    stats preview card banner image added

--- challenge_solutions/stats_preview_card_component.jsx
+++ challenge_solutions/stats_preview_card_component.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const StatsPreviewCardComponent = () => {
-  return <div>stats_preview_card_component</div>;
+  return (
+    <div className="bg-[#090B1A] text-white flex flex-row ">
+      {/* Stats Contianer */}
+      <div>
+        <h1>Get insighes the help your business grow.</h1>
+        <p>
+          Discover the benifits of data analytics and make better decisions regarding revenue, customer experience, and
+          overall efficiency.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default StatsPreviewCardComponent;
