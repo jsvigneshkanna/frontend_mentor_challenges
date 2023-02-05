8085a5e692ac25705958c90c7be0b7d43ad14399
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-06 00:10:50 +0530

    footer made relative in all devices

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div
-      className="relative md:fixed md:left-0 md:bottom-0 text-center bg-slate-200 dark:bg-sky-900 
+      className="text-center bg-slate-200 dark:bg-sky-900 
     py-2 rounded-t-2xl text-sm md:text-base font-semibold w-full "
     >
       Made with ❣️J S Vignesh Kanna | 2022
